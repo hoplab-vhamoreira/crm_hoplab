24057e13dde3c3c88e6a497957a59788f0a74e24
hoplab-vhamoreira <vitor.moreira@hoplab.pt>
2026-05-12 13:22:13 +0100

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -5,7 +5,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const SUPABASE_URL  = 'https://bocwqacwalzshjkhjzwi.supabase.co'
-const SUPABASE_ANON = 'SUPABASE_ANON_KEY_AQUI' // ← substituir antes do primeiro deploy
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvY3dxYWN3YWx6c2hqa2hqendpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MjYyNTYsImV4cCI6MjA5NDAwMjI1Nn0.0FSXkO69PG4na6XLAOSoJ-r2wNQYwyLoOSMIyDH7AvY'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON, {
   auth: {
